e7f79a5f5fe64b971062f01fa9d92a66fe1c05bf
Ishani <ishaniamin@gmail.com>
2018-05-31 17:59:48 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,7 +7,7 @@ def turn_count(board)
       count += 1
     end
   end
-  # count
+  count
 end
 
 
@@ -17,4 +17,18 @@ def current_player(board)
 
   count % 2 == 0 ? "X" : "O"
 
+end
+
+
+
+def turn_count(board)
+  count = 0
+  board.each do |cell|
+    count += 1 if (cell == "X") || (cell == "O")
+  end
+  count
+end
+
+def current_player(board)
+  turn_count(board).odd? ? "O" : "X"
 end
